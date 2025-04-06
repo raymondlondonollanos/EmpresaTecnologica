2c42a065a14d32ff15fa0ae32e9981fb250a57dc
raymondlondonollanos <95382390+raymondlondonollanos@users.noreply.github.com>
2025-04-06 14:51:57 -0500

    main.cpp
    Abril 6 2:51

--- main.cpp
+++ main.cpp
@@ -59,26 +59,7 @@ int main()
 		//limpieza de buffer
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	
-		std::cout << "\nIngresa el numero de items a comprar en nuestras empresa de tecnologia: ";
-		std::cin >> num_items;
-
-		clientes::Clientes cl{p_nombre , p_apellido , ID, num_items};
-
-		//Inicializamos el puntero para almacenar los items ingresado por el usuario
-		items_ptr = new std::string[num_items];
-
-		for (std::size_t i{ 0 }; i < num_items; i++)
-		{
-			//creacion de variable local string 
-			std::string items_local{};
-			
-			std::cout << "\nIngresa el items " << (i + 1) 
-				<< ": ";
-			std::cin >> items_local;
-
-			items_ptr[i] = items_local;
-		}
-
+		
 		cl.agregar_items(items_ptr);
 
 		//limpiamos el puntero, que no necesitamos 
