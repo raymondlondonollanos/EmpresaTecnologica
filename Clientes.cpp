541fafee7676a23e5500e0e98d4a658d7c14d350
Raymond londoño <raymond_5400@hotmail.com>
2025-04-09 22:30:57 -0500

    10:30 10 bril

--- Clientes.cpp
+++ Clientes.cpp
@@ -1,6 +1,7 @@
 #include "Clientes.h"
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 namespace clientes
 {
@@ -85,7 +86,7 @@ namespace clientes
 		std::string* temp_vector;
 		short cant_item_copyV{ 0 };
 		short cant_item_tempV{ 0 };
-		char validador{ 'n' };
+		char validador = 'n';
 
 		std::cout << "Ingrese el numero de items: ";
 		//m_num_items pasa como una variable objeto implicito
@@ -115,7 +116,91 @@ namespace clientes
 					m_vect_nom_prod_in_clien_ptr[i] = item_local;
 				}
 
+
 			}
+			else
+			{
+
+				//validacion que no sea nulo el vector m_vec_nom
+				if (m_vect_nom_prod_in_clien_ptr != nullptr)
+				{
+		
+					cant_item_copyV = m_num_item;
+
+					copy_vector = new std::string[cant_item_copyV];
+
+					//almacenamos la informacion transitoria en este vector copy
+					std::copy(m_vect_nom_prod_in_clien_ptr, m_vect_nom_prod_in_clien_ptr+ m_num_item, copy_vector);
+
+					//borramos el puntero y lo declaramos nulo
+					delete[] m_vect_nom_prod_in_clien_ptr;
+					m_vect_nom_prod_in_clien_ptr = nullptr;
+
+					//Le solicitamos al usuario nuevamente el numero de item a ingresar
+					std::cout << "\nIngresa el numero de items: ";
+					std::cout << cant_item_tempV;
+
+					//inicializamos el vector temporal para tener la otra lista almacenada
+					temp_vector = new std::string[cant_item_tempV];
+
+					//solicitamos ingresar los items al usuario por consola
+					for (std::size_t i{0}; i < cant_item_tempV ; ++i)
+					{
+						//inicio for
+
+						//variable local for
+						std::string item_local_dos;
+
+						std::cout << "\nIngresa los items: ";
+						std::cin >> item_local_dos;
+
+						temp_vector[i] = item_local_dos;
+
+						//fin for
+					}
+
+					//realizamos la suma del valor total almacenado en ambos vec y copiamos a num_item
+					m_num_item = cant_item_tempV + cant_item_copyV;
+
+					//Inicializamos el vector para almacenar los items almacenados en lso dos vectores
+					m_vect_nom_prod_in_clien_ptr = new std::string[m_num_item];
+
+					//empezamos el copiado del primer vector copy
+					for (std::size_t i{0}; i < cant_item_copyV ; ++i)
+					{
+						//inicio del for
+						m_vect_nom_prod_in_clien_ptr[i] = copy_vector[i];
+						
+						//fin ciclo for
+					}
+
+					//Empezamos el copiado de segundo vector tem secuencialmente en el vector m_vect
+					for (std::size_t i {0}; i<cant_item_tempV ; ++i)
+					{
+						//inico for
+						m_vect_nom_prod_in_clien_ptr[cant_item_copyV + i] = temp_vector[i];
+
+						//fin ciclo for
+					}
+
+					delete[] copy_vector;
+					copy_vector = nullptr;
+
+					delete[] temp_vector;
+					temp_vector = nullptr;
+
+				}
+				
+
+			}
+
+			//salimimos del if que valida que m_num_vect no sea nulo
+					//Verificamos antes si el usuario desea agregar mas itema o salir
+			std::cout << "\nDeseas agregar mas items: s/n";
+			std::cin >> validador;
+
+			//limpiar el buffer para evitar caracteres estranos
+			std::cin.ignore();
 
 		} while (validador == 's');
 
--- main.cpp
+++ main.cpp
@@ -32,106 +32,11 @@ int main()
 
 
 
-        std::string* items_ptr;
-        std::string* copy_items_ptr;
-        std::string* tem_copy_items_ptr;
-        char validador = 'n';
-        int num_impu_user;
-        int temp_copy = 0;
+	clientes::Clientes cl{ "Raymond","Llanos",1130343,3};
 
+	cl.agregar_items();
 
-        std::cout << "Ingresa la cantidad de items ";
-        std::cin >> num_impu_user;
-
-        do {
-
-
-
-            if ('s' == validador)
-            {
-
-                temp_copy = num_impu_user;
-
-                copy_items_ptr = new std::string[temp_copy];
-
-                std::copy(items_ptr, items_ptr + num_impu_user,
-                    copy_items_ptr);
-                std::cout << "funciona hasta quiw\n";
-
-
-                delete[] items_ptr;
-                items_ptr = nullptr;
-
-
-                std::cout << "Ingresa la cantidad de items ";
-                std::cin >> num_impu_user;
-
-                tem_copy_items_ptr = new std::string[num_impu_user];
-
-                items_ptr = new std::string[num_impu_user + temp_copy];
-
-
-                for (int i = 0; i < num_impu_user; ++i)
-                {
-                    std::string item;
-                    std::cout << "Ingresa los items: ";
-                    std::cin >> item;
-
-                    tem_copy_items_ptr[i] = item;
-
-                }
-
-                for (int i = 0; i < temp_copy; ++i)
-                {
-
-                    items_ptr[i] = copy_items_ptr[i];
-
-                }
-
-                for (int i = 0; i < num_impu_user; ++i)
-                {
-
-                    items_ptr[temp_copy + i] = copy_items_ptr[i];
-
-                }
-
-
-            }
-            else
-            {
-                items_ptr = new std::string[num_impu_user];
-
-                for (int i = 0; i < num_impu_user; ++i)
-                {
-                    std::string item;
-                    std::cout << "Ingresa los items: ";
-                    std::cin >> item;
-
-                    items_ptr[i] = item;
-
-                }
-
-            }
-
-
-            std::cout << "Deseas agregar mas item s/n";
-            std::cin >> validador;
-
-        } while ('s' == validador);
-
-
-        for (int i = 0; i < temp_copy + num_impu_user; ++i)
-        {
-
-            std::cout << " vector : " << items_ptr[i];
-
-        }
-
-     
-
-	
-
-		
+	cl.mostrar_item();
 
 	return 0;
 }
